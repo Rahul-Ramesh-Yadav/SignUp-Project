47018d42910a1573d972c789089f25115f48f0d4
RahulYadav1319 <rahulyadav.eduworld@gmail.com>
2021-01-17 14:19:14 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -7,35 +7,24 @@ require("./db/conn");
 const Register = require("./models/register");
 const port = process.env.PORT || 3000;
 
-//const static_path = path.join(__dirname,"../public");
-//const template_path = path.join(__dirname,"../templates/views");
-//const partials_path = path.join(__dirname,"../templates/partials");
+const static_path = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partials_path = path.join(__dirname,"../templates/partials");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-//app.use(express.static(static_path));
-//app.set("view engine","hbs");
-//app.set("views",template_path);
-//hbs.registerPartials(partials_path);
+app.use(express.static(static_path));
+app.set("view engine","hbs");
+app.set("views",template_path);
+hbs.registerPartials(partials_path);
 
 app.get("/",(req,res) => {
 res.render("index")
 });
 
-app.get("/register",async(req,res) => {
-    //res.render("register");
-        try{
-            const insertU = await Register.find({});
-            res.status(201).send(insertU);
-        }catch(e){
-            res.status(400).send(e);
-        }
-        //res.render("register");
-
-        //const regis = await Register.find({});
-        //res.status(201).send(regis);
- 
+app.get("/register",(req,res) => {
+    res.render("register");
 })
 
 app.post("/register",async(req,res) => {
@@ -43,9 +32,25 @@ app.post("/register",async(req,res) => {
         //console.log(req.body.firstname);
         //res.send(req.body.firstname);
 
-            const register = new Register(req.body);
-            console.log(req.body);
-            register.save();
+        const password = req.body.password;
+        const confirmPassword = req.body.confirmPassword;
+
+        if(password === confirmPassword){
+            const register = new Register({
+                firstname : req.body.firstname,
+                lastname : req.body.lastname,
+                email : req.body.email,
+                password : req.body.password,
+                confirmPassword : req.body.confirmPassword,
+                mobileNo : req.body.mobileNo,
+                gender : req.body.gender
+            })
+            const registered = await register.save();
+            res.status(201).render("index");
+        }else{
+            res.send("password are not matching");
+        }
+
     } catch(error){
         res.status(400).send(error);
     }
